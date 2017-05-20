4544ffa3c0a18651dc95cdf5f2695ebed583c380
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2017-05-20 14:46:22 +0100

    Add a failing test checking that the item passed to the scan method is not null and that an ArgumentNullException is thrown with an appropriate message if it is.

--- tests/LateRoomsCheckoutKata.Checkout.Tests/CheckoutTests.cs
+++ tests/LateRoomsCheckoutKata.Checkout.Tests/CheckoutTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,21 @@ namespace LateRoomsCheckoutKata.Checkout.Tests
 {
     public class CheckoutTests
     {
+        /// <summary>
+        /// This class contains all tests for the Scan method.
+        /// </summary>
+        [TestFixture]
+        public class TheScanMethod
+        {
+            [Test]
+            public void ShouldThrowAnArgumentNullExceptionWithAnAppropriateMessageWhenTheItemBeingScannedIsNull()
+            {
+                var checkout = new Checkout();
+                Action act = () => checkout.Scan(null);
+                act.ShouldThrow<ArgumentNullException>()
+                    .And.Message.Should()
+                    .Be("A null item has been passed in. The item cannot be null.\r\nParameter name: item");
+            }
+        }
     }
 }
