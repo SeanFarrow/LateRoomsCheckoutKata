67550e2013a3f38a2bc2355c44bd8e905de11039
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2017-05-20 09:20:26 +0100

    Flesh out the ICheckout interface.

--- src/LateRoomsCheckoutKata.Contracts/ICheckout.cs
+++ src/LateRoomsCheckoutKata.Contracts/ICheckout.cs
@@ -1,7 +1,20 @@
 ﻿namespace LateRoomsCheckoutKata.Contracts
 {
+    /// <summary>
+    /// The interface for a supermarket checkout.
+    /// </summary>
     public interface ICheckout
     {
+        /// <summary>
+        /// Scan an item and add it to the list of products we are purchasing.
+        /// </summary>
+        /// <param name="item">The SKU of the item we want to purchase.</param>
+        void Scan(string item);
         
+        /// <summary>
+        /// Retrieve the current running total.
+        /// </summary>
+        /// <returns>The checkout's currently calculated total.</returns>
+        int GetTotalPrice();
     }
 }
