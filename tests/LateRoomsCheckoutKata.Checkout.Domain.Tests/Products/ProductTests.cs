6874e148beca646f02cc4846237b87e09fb74ee6
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2017-05-22 16:07:20 +0100

    Add a class holding tests for the Product class.

---
+++ tests/LateRoomsCheckoutKata.Checkout.Domain.Tests/Products/ProductTests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LateRoomsCheckoutKata.Checkout.Domain.Tests.Products
+{
+    public class TheProductConstructorTests
+    {
+        
+    }
+    public class ProductTests
+    {
+        
+    }
+}
