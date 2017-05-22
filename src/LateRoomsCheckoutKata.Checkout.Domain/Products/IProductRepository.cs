521dab0d50024f2c5b167321f624950d903ab3b2
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2017-05-22 15:30:36 +0100

    Update the FindProductBySKU method of the IProductRepository interface to have xml comments.

--- src/LateRoomsCheckoutKata.Checkout.Domain/Products/IProductRepository.cs
+++ src/LateRoomsCheckoutKata.Checkout.Domain/Products/IProductRepository.cs
@@ -5,6 +5,14 @@
     /// </summary>
     public interface IProductRepository
     {
+        /// <summary>
+        /// Find a product sold by the supermarket.
+        /// </summary>
+        /// <param name="SKU">The stock keeping unit of the <see cref="Product"/> to find.</param>
+        /// <returns>The <see cref="Product"/> if found.</returns>
+        ///<exception cref="ArgumentNullException">The <paramref name="sku"/> passed in is <c>null</c>.</exception>
+        ///<exception cref="ArgumentException">The <paramref name="sku"/> passed in is an empty string.></exception>
+        /// <exception cref="ProductWithSKUNotFoundException"><paramref name="sku"/> The passed in stock keeping unit is no longer sold by the supermarket.</exception>
         Product FindProductBySKU(string SKU);
     }
 }
