b09afe49682597f1bfe062c5f5d2543fcfe83170
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2017-05-23 09:48:05 +0100

    Delete the checkout constructors we no longer require.

--- src/LateRoomsCheckoutKata.Checkout/Checkout.cs
+++ src/LateRoomsCheckoutKata.Checkout/Checkout.cs
@@ -22,27 +22,6 @@ namespace LateRoomsCheckoutKata.Checkout
         /// </summary>
         /// <param name="productRepository">The repository providing access to the <see cref="Product"/>s available in the supermarket.</param>
         /// <param name="productDiscountRuleRepository">The repository providing access to the discount rules for <see cref="Product"/>s in the supermarket.</param>
-        ///<exception cref="ArgumentNullException">The <paramref name="productRepository"/> or <paramref name="productDiscountRuleRepository"/> passed in are <c>null</c>.</exception>
-        public Checkout(IProductRepository productRepository, IProductDiscountRuleRepository productDiscountRuleRepository)
-        {
-            if (productRepository == null)
-            {
-                throw new ArgumentNullException("productRepository", "A null product repository has been passed in. The product repository cannot be null.");
-            }
-            else if (productDiscountRuleRepository == null)
-            {
-                throw new ArgumentNullException("productDiscountRuleRepository", "A null product discount rule repository has been passed in. The product discount rule repository cannot be null.");
-            }
-
-            this._productRepository = productRepository;
-            this._productDiscountRuleRepository = productDiscountRuleRepository;
-        }
-        
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Checkout"/> class.
-        /// </summary>
-        /// <param name="productRepository">The repository providing access to the <see cref="Product"/>s available in the supermarket.</param>
-        /// <param name="productDiscountRuleRepository">The repository providing access to the discount rules for <see cref="Product"/>s in the supermarket.</param>
         /// <param name="till">The till to be used when unit testing the class. If this is <c>null</c> a new till will be assigned to the private field in the constructor.</param>
         ///<exception cref="ArgumentNullException">The <paramref name="productRepository"/> or <paramref name="productDiscountRuleRepository"/> passed in are <c>null</c>.</exception>
         public Checkout(IProductRepository productRepository, IProductDiscountRuleRepository productDiscountRuleRepository, IDictionary<string, uint> till =null)
@@ -61,14 +40,6 @@ namespace LateRoomsCheckoutKata.Checkout
             this._till = till ?? new Dictionary<string, uint>();
             }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Checkout"/> class.
-        /// </summary>
-        public Checkout()
-        {
-            // TODO: Complete member initialization
-        }
-        
         ///<InheritDoc/>
         public void Scan(string item)
         {
