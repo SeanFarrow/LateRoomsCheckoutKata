8527e9a59513468ff33c414aee98e09fe629ebf6
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2017-05-24 14:42:20 +0100

    Add comments to the IProductDiscountRule interface.

--- src/LateRoomsCheckoutKata.Checkout.Domain/Products/IProductDiscountRule.cs
+++ src/LateRoomsCheckoutKata.Checkout.Domain/Products/IProductDiscountRule.cs
@@ -5,8 +5,16 @@
     /// </summary>
     public interface IProductDiscountRule
     {
+        /// <summary>
+        /// The quantity of the product this rule applies to that can be discounted.
+        /// </summary>
         int QuantityToDiscount { get; }
-
+        /// <summary>
+        /// Calculates the discount that is applicable to the <see cref="Product"/> this rule is applicable to.
+        /// </summary>
+        /// <param name="numberOfScannedProducts">The total number of this <see cref="Product"/> that have been scanned throu the till.</param>
+        /// <param name="unitPrice"><The unit price of the <see cref="Product"/> this rule applies to./param>
+        /// <returns>The total price for the discounted portion of the order.</returns>
         int CalculateDiscount(int numberOfScannedProducts, int unitPrice);
     }
 }
